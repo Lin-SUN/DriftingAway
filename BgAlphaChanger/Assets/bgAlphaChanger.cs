27e71c33be8e7ab012c5117901745461bd721d3c
Heisenberg40 <fromash@sina.cn>
2021-04-25 14:08:01 +0800

    增加了SampleScene的测试场景；添加两张mobius背景图，过渡动画及控制脚本。

---
+++ BgAlphaChanger/Assets/bgAlphaChanger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgAlphaChanger : MonoBehaviour
+{
+    public SpriteRenderer blue;
+    public SpriteRenderer orange;
+    private float alphaLevel = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Color blueOrig = new Color(1f, 1f, 1f, 1f);
+        Color blueFinal = new Color(1f, 1f, 1f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+       // alphaLevel -= (Time.deltaTime+0.3f);
+       
+      //  blue.color = new Color(1f, 1f, 1f, alphaLevel);
+    }
+}
